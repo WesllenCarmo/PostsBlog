3b336cdf2e6d95bed25787272dcd6266b82af75a
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-05-06 17:23:29 -0300

    feat: create table Posts in the database with sequelize

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
+
 const express = require("express");
 const app = express();
 
-app.listen(3001, () => {
-  console.log("Server is running on port 3001");
-})
+const db = require("./models");
+
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+  });
+});
