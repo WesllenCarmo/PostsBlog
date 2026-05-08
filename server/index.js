d856ff744f5b49d431b80e20b1236326073851a9
WesllenCarmo <wesllenaraujo.developer@gmail.com>
2026-05-08 16:28:37 -0300

    feat: create get and post methods with express router to insert/show data in the database.

--- server/index.js
+++ server/index.js
@@ -4,7 +4,13 @@ require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const db = require("./models");
+
+// Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
